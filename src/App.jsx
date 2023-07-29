7cc8622ef628bd921e25dec0e9e87c301930d031
Roberta <roberta.amaro89@gmail.com>
2023-07-29 07:17:30 -0300

    add ul

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,36 @@
 import "./App.css";
 
-/* import { ReactComponent as ImageDesktop } from "./components/Image/ImageDesktop.svg"; */
 import { ReactComponent as ImageMobile } from "./components/Image/ImageMobile.svg";
+import { ReactComponent as IconList } from "./assets/images/icon-list.svg";
 
 function App() {
   return (
     <>
       <div className="w-full">
-        {/* <ImageDesktop /> */}
         <ImageMobile className="w-full h-auto object-cover" />
       </div>
-      <h1 className="title font-bold text-4xl text-left text-hsl(234, 29%, 20%)">
-        Stay updated!
-      </h1>
-      <p className="text-left">
-        Join 60,000+ product managers receiving monthly updates on:
-      </p>
+      <section className="flex flex-col p-8 gap-5">
+        <h1 className="title font-bold text-4xl  text-darkGrey">
+          Stay updated!
+        </h1>
+        <p className="text-base font-normal   text-darkGrey">
+          Join 60,000+ product managers receiving monthly updates on:
+        </p>
+      </section>
+      <ul className="list flex flex-col px-9 gap-3">
+        <div className="icons flex gap-5">
+          <IconList />
+          <li>Product discovery and building what matters</li>
+        </div>
+        <div className="icons flex gap-5">
+          <IconList />
+          <li>Measuring to ensure updates are a sucess</li>
+        </div>
+        <div className="icons flex gap-5">
+          <IconList />
+          <li>And much more!</li>
+        </div>
+      </ul>
     </>
   );
 }
