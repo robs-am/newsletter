cfa199be18d09633a8fb2b0ba10e41c013136741
Roberta <roberta.amaro89@gmail.com>
2023-07-29 00:23:18 -0300

    inicia configuração e inclusão de componentes de imagem

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import ImageDesktop from "./components/Image/ImageDesktop";
+import ImageMobile from "./components/Image/ImageMobile";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1>Hello!</h1>
+      <ImageMobile />
+      <ImageDesktop />
     </>
   );
 }
---
+++ src/components/Image/ImageDesktop.jsx
@@ -0,0 +1,7 @@
+import imageDesktop from "./imageDesktop.svg";
+
+const ImageDesktop = () => {
+  return <img src={imageDesktop} />;
+};
+
+export default ImageDesktop;
